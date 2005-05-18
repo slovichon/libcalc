c4c37bb42bb4476fe0a3a03d09c4014d28f6ca5d
yanovich <>
2005-05-18 00:27:30 +0000

    cleanup

--- expr.c
+++ expr.c
@@ -1,82 +1,79 @@
-#include "calclib.h"
+/* $Id$ */
+
 #include <stdio.h>
 #include <stdarg.h>
 
-static void bark(char *msg, ...)
+#include "calclib.h"
+
+struct cl_expr *
+cl_expr_init(void)
 {
-	va_list ap;
-	fprintf(stderr, "[ERR] ");
-	va_start(ap, msg);
-	vfprintf(stderr, msg, ap);
-	va_end(ap);
-	fprintf(stderr, "\n");
-	fflush(stderr);
+	expr_t *expr;
+
+	if ((expr = malloc(sizeof(*expr))) == NULL)
+		return (NULL);
+	expr->terms = NULL;
+	expr->num_terms = 0;
+	return (expr);
 }
 
-Expression *Expression_init()
+void
+expr_free(expr_t *expr)
 {
-	Expression *p;
-	p = xmalloc(sizeof(Expression));
-	p->terms = NULL;
-	p->num_terms = 0;
-	return p;
+	term_t **t;
+
+	if (expr == NULL)
+		return;
+	for (t = expr->terms; *t != NULL; t++)
+		free(t);
+	free(expr);
 }
 
-void Expression_free_(Expression **p, char *file, int line)
+expr_t *
+expr_parse(char *s, int len)
 {
-	if (p && *p) {
-		Term **t;
-		for (t = p->terms; *t != NULL; t++)
-			free(t);
-		free(*p);
-		*p = NULL;
-	}
-}
-
-Expression *Expression_parse_str(char *s, int len)
-{
-	Expression *p;
+	expr_t *expr;
 	int function_level = 0;
 	int integral_level = 0;
 	int derivative_level = 0;
 	char *q;
 
-	p = Expression_init();
+	expr = Expression_init();
 
 	for (q = s; (s != '\0') && (q-s < len); s++) {
 		switch (*q) {
-			case '(': {
-				/* Parse sub-expression */
-				char *t;
-				int subexprs = 1;
-				for (t = q; *t != '\0'; t++)
-					switch (*t) {
-						case '(':
-							subexprs++;
-							break;
-						case ')':
-							if (--subexprs < 0)
-								/* Parse error */
-								return;
-							else if (subexprs == 0)
-								break;
-							break;
-					}
-				/* t now points to end of sub expression */
-				subexpr = Expression_parse_str(q, t-q);
-				q = t+1;
-				break;
-			}
-			case '-': case '0': case '1': case '2': case '3':
-			case '4': case '5': case '6': case '7': case '8':
-			case '9': case '.': {
-				break;
-			}
-			case ' ':
-				break;
-			default:
+		case '(': {
+			/* Parse sub-expression */
+			char *t;
+			int subexprs = 1;
+			for (t = q; *t != '\0'; t++)
+				switch (*t) {
+				case '(':
+					subexprs++;
+					break;
+				case ')':
+					if (--subexprs < 0)
+						/* Parse error */
+						return;
+					else if (subexprs == 0)
+						break;
+					break;
+				}
+			/* t now points to end of sub expression */
+			subexpr = expr_parse(q, t-q);
+			q = t+1;
+			break;
+		}
+		case '-': case '0': case '1': case '2': case '3':
+		case '4': case '5': case '6': case '7': case '8':
+		case '9': case '.': {
+			break;
+		}
+		case ' ':
+			break;
+		default:
 		}
 	}
 
-	return p;
+	return (expr);
 }
